2698098cc5391eae7d60990d44e6d1fd9fcad04d
Trey <tbeau5595@hotmail.com>
2019-02-14 22:36:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,35 @@
 def new_hash
-
+  {}
 end
 
 def actor
-
+actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+	  :railroads  => {
+	    :pieces => 4,
+	    :rent_in_dollars => {
+	      :one_piec_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 75,
+	      :four_pieces_owned => 200
+	},
+	:name => {
+	  :reading_railroad =>
+	  {"mortgage_value" => "$100"
+	  },
+	  :pennsylvania_railroad =>
+	  {"mortgage_value" => "$200"
+	  },
+	  :b_and_o_railroad => 
+	  {"mortgage_value" => "$400"
+	  },
+	  :shortline => 
+	  {"mortgage_value" => "$800"
+	  }
+	}
 
 end
 
